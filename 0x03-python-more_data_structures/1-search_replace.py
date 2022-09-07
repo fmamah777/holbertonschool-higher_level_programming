db1b224fa5d86e15e9ac2b36a349937e6b818367
fmamah777 <fm.mamah@gmail.com>
2022-09-06 20:13:53 -0700

    corrected commits

--- 0x03-python-more_data_structures/1-search_replace.py
+++ 0x03-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    return [i if i != search else replace for i in my_list]
-
+    return list(map(lambda x: x if x != search else replace, my_list))
