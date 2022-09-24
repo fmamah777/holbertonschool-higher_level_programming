25ee1b6e42254ccc02d9a44052366c03a78d3e2a
fmamah777 <fm.mamah@gmail.com>
2022-09-24 15:34:03 -0700

    fixes

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,26 +1,13 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
-"""
-script list states with 'a' in its name
-"""
-from sys import argv
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+""" Module 9 student. """
 
 
-if __name__ == "__main__":
-    dbengine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                             .format(argv[1], argv[2], argv[3]),
-                             pool_pre_ping=True)
+class Student:
+    """ Defines a student class. """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-    Base.metadata.create_all(dbengine)
-    dbsess = sessionmaker(bind=dbengine)
-
-    Astates = dbsess().query(State).order_by(State.id)\
-        .filter(State.name.like("%a%"))
-
-    for row in Astates:
-        print("{}: {}".format(row.id, row.name))
-
-    dbsess().close
+    def to_json(self):
+        return vars(self)
