83aed2ea7090cdd2620ff15227cdcf26ee6fc0d2
fmamah777 <fm.mamah@gmail.com>
2022-09-24 23:36:33 -0700

    bug fixes

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This module contains the class: Base.
+ contains Base class.
 """
 import json
 import os
@@ -8,23 +8,21 @@ import os
 
 class Base:
     """
-    This class is the base for all classes that can
-    inherit from it.
-    nb_objects is a private attribute that identidies objects
-    created without one. (an i.d)
+    base class to inherit from nb_objects. used to identidify objects
+    created without an i.d. class
     """
     __nb_objects = 0
 
     @classmethod
     def clear(cls):
         """
-        resets the nb_objects.
+        resets  nb_objects.
         """
         Base.__nb_objects = 0
 
     def __init__(self, id=None):
         """
-        This method ignores the object.
+        This ignores the object.
         """
         if id is None:
             Base.__nb_objects += 1
@@ -35,8 +33,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """
-        returns JSON string representiation of
-        a dictionary, 'list_dictionaries'.
+        returns 'list_dictionaries'.
         """
         if not list_dictionaries:
             return "[]"
@@ -45,8 +42,7 @@ class Base:
     @classmethod
     def save_to_file(cls, list_objs):
         """
-        JSON string representation of an
-        object, 'list_objs', to a file.
+        JSON representation of 'list_objs', to file.
         """
         filename = cls.__name__ + ".json"
         newlist = []
@@ -59,7 +55,7 @@ class Base:
     @staticmethod
     def from_json_string(json_string):
         """
-        returns the list of the string, 'json_string'.
+        returns list from json_string.
         """
         if not json_string:
             return []
@@ -68,8 +64,7 @@ class Base:
     @classmethod
     def create(cls, **dictionary):
         """
-        returns object set by
-        kwargs, 'dictionary'.
+        object returns in kwargs, 'dictionary'.
         """
         if cls.__name__ == "Rectangle":
             new_obj = cls(1, 1)
@@ -81,8 +76,7 @@ class Base:
     @classmethod
     def load_from_file(cls):
         """
-        returns a list of instances in a specific
-        class, 'cls'.
+        returns instance 'cls'.
         """
         filename = cls.__name__ + ".json"
         if not os.path.exists(filename):
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -4,7 +4,7 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """ class inherits from rectangle """
+    """ class taken/inherits from rectangle """
     def __init__(self, size, x=0, y=0, id=None):
         """ Init/initialize  square """
         super().__init__(size, size, x, y, id)
