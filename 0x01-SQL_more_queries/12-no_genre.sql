7b6280d9a315385354f61d0778e609d58dca00ba
fmamah777 <fm.mamah@gmail.com>
2022-10-06 13:43:11 -0700

    final commit

---
+++ 0x01-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,6 @@
+-- find a tv show and group them together
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+    LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+    WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
---
+++ 0x01-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,9 @@
+-- finds given records
+SELECT id, name
+FROM cities
+WHERE state_id IN (
+    SELECT id
+    FROM states
+    WHERE name = 'California'
+)
+ORDER BY id ASC;
