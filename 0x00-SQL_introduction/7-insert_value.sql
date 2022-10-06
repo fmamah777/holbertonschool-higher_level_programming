9281856d0aeb7560253cc136e8442962b32039df
fmamah777 <fm.mamah@gmail.com>
2022-10-06 02:09:49 -0700

    late commits

--- 0x00-SQL_introduction/7-insert_value.sql
+++ 0x00-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
--- Writing a script that inserts a new row in first_table
-
-insert into 'first_table' ('id', 'name') values ("89", "Best School");
+-- Inserts a new row in the table first_table.
+-- Syntax: INSERT [LOW_PRIORITY | DELAYED | HIGH_PRIORITY] [IGNORE][INTO] tbl_name
+INSERT INTO first_table (id, name) VALUES(89, 'Best School');
