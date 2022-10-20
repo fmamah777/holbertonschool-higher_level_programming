922e165ba280a6ddd119cdc0433467f8ee554f30
fmamah777 <fm.mamah@gmail.com>
2022-10-19 19:55:04 -0700

    error correction

--- 0x0B-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0B-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,30 +1,17 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
 """
-a script that lists all State objects that contain the letter a from the database hbtn_0e_6_usa
+contains city class
 """
+from sqlalchemy import Column, ForeignKey, Integer, String
+from model_state import Base
 
-if __name__ == '__main__':
 
-    import sys
-    import sqlalchemy
-    from sys import argv
-    from sqlalchemy.orm import sessionmaker
-    from model_state import Base, State
-    from sqlalchemy.orm import Session
-    from sqlalchemy import create_engine
+class City(Base):
+    """
+    class city that contains the cities
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
 
-    vtech = \
-        create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
-                      sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    
-    
-    Base.metadata.create_all(vtech)
-    Session = sessionmaker(bind=vtech)
-    session = Session()
-    novelty_state = session.query(State).filter_by(State.name.like('%a%'))
-    for state in novelty_state:
-        session.delete(state)
-    session.commit()
-    session.close()
