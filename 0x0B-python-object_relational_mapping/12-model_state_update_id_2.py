a729acae85a14a42be0c164066f6d0fa3e223464
fmamah777 <fm.mamah@gmail.com>
2022-10-19 19:39:45 -0700

    pycode

--- 0x0B-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0B-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,29 +1,24 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
 """
-a script that lists all State objects that contain the letter a from the database hbtn_0e_6_usa
+changes the name of the to given where id is a specific number
 """
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 
-if __name__ == '__main__':
 
-    import sys
-    import sqlalchemy
-    from sys import argv
-    from sqlalchemy.orm import sessionmaker
-    from model_state import Base, State
-    from sqlalchemy.orm import Session
-    from sqlalchemy import create_engine
+if __name__ == "__main__":
+    dbengine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                             .format(argv[1], argv[2], argv[3]),
+                             pool_pre_ping=True)
 
-    vtech = \
-        create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
-                      sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    
-    
-    Base.metadata.create_all(vtech)
-    Session = sessionmaker(bind=vtech)
-    session = Session()
-    novelty_state = session.query(State).filter_by(id=2).first()
-    novelty_state.name = "New Mexico"
-    session.commit()
-    session.close()
+    Base.metadata.create_all(dbengine)
+    dbsess = sessionmaker(bind=dbengine)
+    dbs = dbsess()
+
+    upstate = dbs.query(State).filter(State.id == 2).one()
+    upstate.name = "New Mexico"
+    dbs.commit()
+
+    dbsess().close
