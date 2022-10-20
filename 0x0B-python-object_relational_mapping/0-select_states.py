00561570ec233be2c049f037ebed71c673327723
fmamah777 <fm.mamah@gmail.com>
2022-10-19 18:01:17 -0700

    extra attempts

--- 0x0B-python-object_relational_mapping/0-select_states.py
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -1,28 +1,15 @@
 #!/usr/bin/python3
-"""
-This script lists all `states` from the database
-`hbtn_0e_0_usa`.
-"""
+""" Lists all states in database """
 import MySQLdb
-import sys
-
-
-def list_states():
-    """
-    lists all states
-    """
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3])
-    cur = db.cursor()
-
-    cur.execute("SELECT * FROM states ORDER BY id")
-    results = cur.fetchall()
-    for row in results:
-        print(row)
-
-    cur.close()
-    db.close()
-
+from sys import argv
 
 if __name__ == "__main__":
-    list_states()
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    db.close()
