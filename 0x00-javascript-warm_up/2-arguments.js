4f864847a6b7e476a821802daee9b303a812a6db
fmamah777 <fm.mamah@gmail.com>
2022-12-01 21:30:39 -0800

    begininng test

---
+++ 0x00-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const args = process.argv;
+const len = args.length;
+if (len === 2) {
+  console.log('No argument');
+} else if (len === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
